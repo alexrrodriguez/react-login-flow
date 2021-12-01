1fff7cd48b78867a3005c019dde8d60af6f838a5
alex <arodriguez202020@gmail.com>
2021-12-01 15:52:33 -0600

    change Login component to Form and use ternary operators to render Login or Register button and Confirm Password input

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
-import Login from "./Login";
+import Form from "./Form";
 
-var isLoggedIn = false;
+var userIsRegistered = false;
 
 function App() {
-  return <div className="container">{isLoggedIn ? <h1>Hello</h1> : <Login />}</div>;
+  return (
+    <div className="container">
+      <h1>Welcome</h1>
+      <Form isRegistered={userIsRegistered} />
+    </div>
+  );
 }
 
 export default App;
